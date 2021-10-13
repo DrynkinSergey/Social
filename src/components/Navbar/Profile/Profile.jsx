0fa44c0f8e643f10de59e4127482247d00d44f70
Sergey <sergsmile29@gmail.com>
2021-10-13 20:45:35 +0300

    Fatal error

--- src/components/Navbar/Profile/Profile.jsx
+++ src/components/Navbar/Profile/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () =>{
         <div className={s.content}>
             <div>
                 <img src="https://www.techrepublic.com/a/hub/i/r/2021/03/08/3f6720de-e2a9-4213-b40f-1889436d92df/resize/1200x/694311317277dbe6af470ea7ea13b38e/monsitj.jpg" alt=""/></div>
-            <div>discription</div>
+            <div>discriptiodn</div>
 
         </div>
 
