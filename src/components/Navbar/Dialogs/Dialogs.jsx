404f77d56d6efe7a8d3b0a78246480eed7ca4d4a
Sergey <sergsmile29@gmail.com>
2021-10-19 01:58:04 +0300

    added 2 components for dialogs

--- src/components/Navbar/Dialogs/Dialogs.jsx
+++ src/components/Navbar/Dialogs/Dialogs.jsx
@@ -1,39 +1,55 @@
 import React from "react";
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+
+const DialogItem = (props) => {
+    let path = "/Dialogs/" + props.id;
+    return(
+
+    <li className={s.item}>
+        <NavLink to={path}>{props.name}</NavLink>
+    </li>
+    );
+
+}
+const MessageSent = (props) => {
+    return(
+            <div className={`${s.message} ${s.sent} ${s.custom}`}>
+                {props.messages}
+            </div>
+    );
+}
+const MessageIncome = (props) => {
+    return(
+        <div className={`${s.message} ${s.income} ${s.custom}`}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum cumque deleniti doloribus eligendi eum fugit magnam nemo, nesciunt nulla obcaecati ullam ut! Corporis cupiditate dignissimos facere, nemo pariatur rerum?
+           <p>{props.messages}</p>
+        </div>
+    );
+}
 
 const Dialogs = () =>{
+
     return(
         <div className={s.dialogsWrapper}>
             <div className={s.friendsList}>
                 <h2>Контакты</h2>
                 <div>
-                    <ul>
-                    <li className={`${s.item} ${s.active}`}>Сергей</li>
-                    <li className={s.item}>Алена</li>
-                    <li className={s.item}>Настя</li>
-                    <li className={s.item}>Алиса</li>
-                    <li className={s.item}>Толик</li>
-                    <li className={s.item}>Оля</li>
-                    <li className={s.item}>Дима</li>
-                    <li className={s.item}>Влад</li>
-                    <li className={s.item}>Миша</li>
-                    <li className={s.item}>Леша</li></ul>
+                        <DialogItem name='Сергей' id='1'/>
+                        <DialogItem name='Настя' id='2'/>
+                        <DialogItem name='Алиса' id='3'/>
+                        <DialogItem name='Толик' id='4'/>
+                        <DialogItem name='Дима' id='5'/>
                 </div>
             </div>
             <div className={s.dialog}>
                 <h2>Сообщения</h2>
-                <div className={`${s.message} ${s.sent} ${s.custom}`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores delectus dolore eaque explicabo hic illum in, libero minus, nam neque odit quasi quia quidem, repellendus temporibus tenetur unde vero voluptatem!</div>
-                <div className={`${s.message} ${s.income} ${s.custom}`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem beatae corporis distinctio dolore enim error expedita, fuga illo illum impedit iste natus non odio placeat provident! Dolorum ex optio vero.</div>
-                <div className={`${s.message} ${s.sent} ${s.custom}`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad, aliquam assumenda debitis dolores eveniet ex fugit in ipsum nulla perspiciatis placeat provident repudiandae sunt tempora. Accusantium laudantium nisi sapiente!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores delectus dolore eaque explicabo hic illum in, libero minus, nam neque odit quasi quia quidem, repellendus temporibus tenetur unde vero voluptatem!</div>
-                <div className={`${s.message} ${s.income} ${s.custom}`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem beatae corporis distinctio dolore enim error expedita, fuga illo illum impedit iste natus non odio placeat provident! Dolorum ex optio vero.</div>
-                    <div className={`${s.message} ${s.income} ${s.custom}`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem beatae corporis distinctio dolore enim error expedita, fuga illo illum impedit iste natus non odio placeat provident! Dolorum ex optio vero.</div>
-                <div className={`${s.message} ${s.sent} ${s.custom}`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores delectus dolore eaque explicabo hic illum in, libero minus, nam neque odit quasi quia quidem, repellendus temporibus tenetur unde vero voluptatem!</div>
-                <div className={`${s.message} ${s.income} ${s.custom}`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem beatae corporis distinctio dolore enim error expedita, fuga illo illum impedit iste natus non odio placeat provident! Dolorum ex optio vero.</div>
-
-                    <div className={`${s.message} ${s.sent} ${s.custom}`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores delectus dolore eaque explicabo hic illum in, libero minus, nam neque odit quasi quia quidem, repellendus temporibus tenetur unde vero voluptatem!</div>
-                    <div className={`${s.message} ${s.sent} ${s.custom}`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores delectus dolore eaque explicabo hic illum in, libero minus, nam neque odit quasi quia quidem, repellendus temporibus tenetur unde vero voluptatem!</div>
-                    <div className={`${s.message} ${s.sent} ${s.custom}`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores delectus dolore eaque explicabo hic illum in, libero minus, nam neque odit quasi quia quidem, repellendus temporibus tenetur unde vero voluptatem!</div>
-
+               <MessageSent messages={'lorloremloremloremloremloremloremloremloremloremloremloremloremem'}/>
+               <MessageSent messages={'            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum cumque deleniti doloribus eligendi eum fugit magnam nemo, nesciunt nulla obcaecati ullam ut! Corporis cupiditate dignissimos facere, nemo pariatur rerum?\n'}/>
+               <MessageIncome messages='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aperiam at, esse exercitationem fugit iusto laborum, minus molestiae nemo obcaecati perspiciatis quas ratione repellat rerum sint sunt tenetur! Magnam, vel?'/>
+               <MessageIncome messages='lorasfafsfasfas fasdddddddddddddddsasdasdadadadddddddd ddddddddddddddddddddem'/>
+               <MessageIncome messages='lorasfafsfasfasfasddddd ddddddddddsasdasdada dadddddddddddddd ddddddddddddddem'/>
             </div>
 
         </div>
