b532bd0af8811aa0cab314e42860d202ae4da935
Sergey <sergsmile29@gmail.com>
2021-11-23 01:03:35 +0200

    add componentDidMount, class component

--- src/components/Navbar/Users/Users.jsx
+++ src/components/Navbar/Users/Users.jsx
@@ -2,43 +2,47 @@ import React from "react";
 import s from './users.module.css';
 import axios from "axios";
 
-const Users = (props) => {
+class Users extends React.Component {
 
-    if (props.users.length === 0) {
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
-                props.setUsers(response.data.items)
+                this.props.setUsers(response.data.items)
             });
     }
-    return <div className={s.wrapper}>
 
-        <div>
-            <h1>Друзья</h1>
-            <hr/>
-            {
-                props.users.map(user =>
-                    <div className={s.userItem} key={user.id}>
-                        <div>
-                            {user.status ? <img className={s.img} src={user.imgSrc} alt=""/> :
-                                <img className={s.img} src={user.statusOfline} alt=""/>}
-                            <div className={s.info}><p className={s.userName}>{user.name}</p>
-                                <p className={s.location}>'user.location.city, user.location.country'</p></div>
-                        </div>
-                        <div className={s.btn}>
-                            {user.followed ? <button onClick={() => {
-                                props.follow(user.id)
-                            }}>Подписаться</button> : <button onClick={() => {
-                                props.follow(user.id)
-                            }}>Удалить</button>}
-                        </div>
-                    </div>
-                )
-            }
-        </div>
-        <div className={s.searchPanel}>
-            Поиск
-        </div>
-    </div>
+    render() {
+        return <div className={s.wrapper}>
+            <div>
 
+                <h1>Друзья</h1>
+                <hr/>
+                {
+                    this.props.users.map(user =>
+                        <div className={s.userItem} key={user.id}>
+                            <div>
+                                {user.status ? <img className={s.img} src={user.imgSrc} alt=""/> :
+                                    <img className={s.img} src={user.statusOfline} alt=""/>}
+                                <div className={s.info}><p className={s.userName}>{user.name}</p>
+                                    <p className={s.location}>'user.location.city, user.location.country'</p></div>
+                            </div>
+                            <div className={s.btn}>
+                                {user.followed ? <button onClick={() => {
+                                    this.props.follow(user.id)
+                                }}>Подписаться</button> : <button onClick={() => {
+                                    this.props.follow(user.id)
+                                }}>Удалить</button>}
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+            <div className={s.searchPanel}>
+                Поиск
+            </div>
+        </div>
+
+    }
 }
+
 export default Users;
